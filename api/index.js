43c65a23c513b270b1737f8db528a6abd885ad1a
hamadvitor0-cell <hamadvitor0@gmail.com>
2026-05-15 23:02:34 -0300

    Expose API startup failure for deploy triage

--- api/index.js
+++ api/index.js
@@ -1,3 +1,17 @@
-const app = require("../backend/app");
+let app;
+
+try {
+  app = require("../backend/app");
+} catch (error) {
+  console.error("[startup]", error);
+  app = (req, res) => {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify({
+      message: "Falha ao iniciar API.",
+      error: error.message
+    }));
+  };
+}
 
 module.exports = app;
